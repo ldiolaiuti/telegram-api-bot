d1c28b4a223ab139ef83b91d1d5c3e0475a6be1d
Luca Diolaiuti <l.diolaiuti@gmail.com>
2023-04-15 23:44:56 +0200

    refactored telegram service

--- src/main/java/com/ldiolaiuti/telegram/api/bot/services/TelegramService.java
+++ src/main/java/com/ldiolaiuti/telegram/api/bot/services/TelegramService.java
@@ -3,28 +3,27 @@ package com.ldiolaiuti.telegram.api.bot.services;
 import com.ldiolaiuti.telegram.api.bot.models.dtos.TelegramParams;
 import com.ldiolaiuti.telegram.api.bot.repositories.TelegramRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class TelegramService {
 
-
-    private final static String TELEGRAM_BOT_USERNAME = "test_ldiolaiuti_bot";
-    private final static String TELEGRAM_BOT_TOKEN = "6005519215:AAG9frXRs4O15AE-8HglpuUqwbL-8T7FOd4";
-
-    private final static String CHAT_ID = "237896500";
+    private final String chatId;
 
     private final TelegramRepository telegramRepository;
 
-    public TelegramService(TelegramRepository telegramRepository) {
+    public TelegramService(@Value("${telegram.bot.chatId}") String chatId,
+                           TelegramRepository telegramRepository) {
+        this.chatId = chatId;
         this.telegramRepository = telegramRepository;
     }
 
     public void sendMessage() {
         String s = telegramRepository.sendMessage(
                 TelegramParams.builder()
-                        .chat_id(CHAT_ID)
+                        .chat_id(chatId)
                         .text("Test message")
                         .build());
         log.info("Sent message: " + s);
