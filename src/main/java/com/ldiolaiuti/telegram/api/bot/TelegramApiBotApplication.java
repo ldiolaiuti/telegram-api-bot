7fb94a085a5a96edf89b43b3189b18aaefa54b2f
Luca Diolaiuti <l.diolaiuti@gmail.com>
2023-04-15 15:10:46 +0200

    scaffolding commit

---
+++ src/main/java/com/ldiolaiuti/telegram/api/bot/TelegramApiBotApplication.java
@@ -0,0 +1,13 @@
+package com.ldiolaiuti.telegram.api.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramApiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramApiBotApplication.class, args);
+	}
+
+}
