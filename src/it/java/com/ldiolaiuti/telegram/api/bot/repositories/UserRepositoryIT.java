01cf0f2806e791d445027a0c91da2c39270ee8d4
Luca Diolaiuti <l.diolaiuti@gmail.com>
2023-04-15 16:58:07 +0200

    exists by username api + test

---
+++ src/it/java/com/ldiolaiuti/telegram/api/bot/repositories/UserRepositoryIT.java
@@ -0,0 +1,38 @@
+package com.ldiolaiuti.telegram.api.bot.repositories;
+
+import com.ldiolaiuti.telegram.api.bot.utils.DbSetupExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("it")
+@ExtendWith(DbSetupExtension.class)
+class UserRepositoryIT {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void shouldExistsByUsername() {
+        boolean exists = userRepository.existsByUsernameIgnoreCase("TestUser");
+        assertThat(exists).isTrue();
+    }
+
+    @Test
+    void shouldExistsByUsernameIgnoreCase() {
+        boolean exists = userRepository.existsByUsernameIgnoreCase("testuser");
+        assertThat(exists).isTrue();
+    }
+
+    @Test
+    void shouldNotExistsByUsername() {
+        boolean exists = userRepository.existsByUsernameIgnoreCase("NewUser");
+        assertThat(exists).isFalse();
+    }
+
+}
