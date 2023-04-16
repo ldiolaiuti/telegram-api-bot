eb42b5822f0cc151d5841f15c66e3c1eead355af
ldiolaiuti <l.diolaiuti@gmail.com>
2023-04-16 23:02:27 +0200

    Revert "Feature/telegram service"

--- src/main/java/com/ldiolaiuti/telegram/api/bot/exceptions/InvalidTokenException.java
+++
@@ -1,8 +0,0 @@
-package com.ldiolaiuti.telegram.api.bot.exceptions;
-
-public class InvalidTokenException extends RuntimeException{
-    public InvalidTokenException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/ldiolaiuti/telegram/api/bot/exceptions/TelegramException.java
+++
@@ -1,8 +0,0 @@
-package com.ldiolaiuti.telegram.api.bot.exceptions;
-
-public class TelegramException extends RuntimeException{
-    public TelegramException(String message) {
-        super(message);
-    }
-
-}
